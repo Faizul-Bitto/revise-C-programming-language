f57834665b8947b9953e4ed536bd52c72bafdccb
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-10-07 22:26:13 +0600

    feat(loop): add printing of even and odd numbers up to 10
    - Implement loop to print even numbers from 0 to 10 inclusive
    - Implement loop to print odd numbers from 1 to 10 inclusive
    - Add newline separator between even and odd number outputs
    - Update output.txt to reflect the new printed numbers

--- 06-loop/a-for-loop/for-loop.c
+++ 06-loop/a-for-loop/for-loop.c
@@ -20,5 +20,19 @@ int main()
         printf("Hello World!\n");
     }
 
+    //! print only even numbers upto 10
+    for (int i = 0; i <= 10; i += 2)
+    {
+        printf("%d\n", i);
+    }
+
+    printf("\n");
+
+    //! print only odd numbers upto 10
+    for (int i = 1; i <= 10; i += 2)
+    {
+        printf("%d\n", i);
+    }
+
     return 0;
 }
