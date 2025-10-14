764f117deac29487801180a833f1e5af3deaecca
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-10-14 21:57:12 +0600

    feat(array-declaring): add C program demonstrating array declaration
    Add a new C program showing how to declare and initialize arrays with examples
    and comments explaining the syntax. Includes input/output files and executable.

---
+++ 08-array/a-array-declaring/array-declare.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int array[5] = {1, 2, 3, 4, 5};
+
+    printf("%d", array);
+
+    return 0;
+}
+
+/*
+    ! To initiate an array :
+    We need to first declare the 'data type'
+    Then we need to declare 'the name of the array'
+    Then in the third bracket '[]', we have to declare the size of that array
+
+    int array[5];
+
+    ! To declare an array directly :
+    We need to first declare the 'data type'
+    Then we need to declare 'the name of the array'
+    Then in the third bracket '[]', we have to declare the size of that array
+    Then, asign the value with third bracket '{}'
+
+    int array[5] = {1, 2, 3, 4, 5};
+
+    We can also declare different data types like float, double etc. if we want
+*/
