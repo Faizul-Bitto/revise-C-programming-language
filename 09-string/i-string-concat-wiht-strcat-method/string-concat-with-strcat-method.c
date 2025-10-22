7f4f693ad6912e8eb81357172fd6ba82a0eee83a
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-10-22 15:36:52 +0600

    feat(string-concat): add input and output files for string concatenation example
    - Introduce input and output text files for testing the string concatenation functionality in C.
    - Add a C program that concatenates two strings using the `strcat` method.
    - This addition enhances the string manipulation examples in the project.

---
+++ 09-string/i-string-concat-wiht-strcat-method/string-concat-with-strcat-method.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char a[1001], b[1001];
+    scanf("%s %s", &a, &b);
+
+    strcat(a, b);
+
+    printf("%s %s", a, b);
+
+    return 0;
+}
