05e8f9bf683f4121cb632761fd4aceb878065ea8
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-10-22 21:14:31 +0600

    feat(string-compare): add C program for manual string comparison with input/output files
    - Introduce a C program that manually compares two strings character by character.
    - Add input and output text files for testing the string comparison functionality.
    - This addition enhances the string manipulation examples in the project.

---
+++ 09-string/j-string-compare-manual/string-compare-manual.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a[1001], b[1001];
+    scanf("%s %s", &a, &b);
+
+    int i = 0;
+
+    while (true)
+    {
+        if ((a[i] == '\0') && (b[i] == '\0'))
+        {
+            printf("Equal");
+            break;
+        }
+        else if (a[i] == '\0')
+        {
+            printf("A is smaller");
+            break;
+        }
+        else if (b[i] == '\0')
+        {
+            printf("B is smaller");
+            break;
+        }
+        else if (a[i] < b[i])
+        {
+            printf("A is smaller");
+            break;
+        }
+        else if (a[i] > b[i])
+        {
+            printf("B is smaller");
+            break;
+        }
+        else if (a[i] == b[i])
+        {
+            i++;
+        }
+    }
+
+    return 0;
+}
