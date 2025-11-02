0148cd7616f2a47a7b34f1cb0f90e589d4471c5d
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-02 16:38:34 +0600

    feat(sum-of-two-values): add C program and files to determine if two values sum to a given number
    - Introduce a C program that checks if any two numbers from a user-provided list sum to a specified value.
    - Add input and output text files for testing the functionality of the program.
    - This addition enhances the problem-solving examples in the project.

---
+++ 11-sum-of-two-values-equal-of-x/sum--of-two-values-equal-of-X.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int x;
+    scanf("%d", &x);
+    int flag = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] + a[j] == x)
+            {
+                flag = 1;
+            }
+        }
+    }
+
+    if (flag == 1)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+
+    return 0;
+}
