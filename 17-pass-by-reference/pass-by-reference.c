5cf0a940928e30e691af4b296efe2446ebec0157
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-05 12:43:54 +0600

    feat(pass-by-value-and-reference): add C programs demonstrating pass-by-value and pass-by-reference
    - Introduce two C programs illustrating the concepts of pass-by-value and pass-by-reference.
    - Include input and output text files for testing the functionality of each program.
    - This addition enhances educational resources related to function parameter passing in C programming.

---
+++ 17-pass-by-reference/pass-by-reference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void func(int *x)
+{
+    *x = 20;
+}
+
+int main()
+{
+    int x = 10;
+
+    func(&x);
+
+    printf("%d", x);
+
+    return 0;
+}
