f0722f0f2c50118dfd2c219616ed7d804398e937
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-05 17:02:24 +0600

    feat(string-functions): add C program for string input and length calculation
    - Introduce a C program that takes a string input from the user, prints the string, and calculates its length.
    - Include input and output text files for testing the functionality of the program.
    - This addition enhances educational resources related to string manipulation and function usage in C programming.

---
+++ 13-functions/h-function-with-string/string-input-and-print.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void func(char s[])
+{
+    printf("%s\n", s);
+
+    int lengthOfString = strlen(s);
+
+    printf("%d", lengthOfString);
+}
+
+int main()
+{
+    char s[10];
+
+    scanf("%s", s);
+
+    func(s);
+
+    return 0;
+}
