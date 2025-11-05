70903243acb3898a9f12d1d34d31344356b588b9
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-05 11:18:50 +0600

    feat(pointer-variable): add C program to demonstrate pointer variable usage
    - Introduce a C program that illustrates how to use pointer variables to store and print memory addresses.
    - Include input and output text files for testing the program's functionality.
    - This addition enhances educational resources related to pointers and memory management in C programming.

---
+++ 15-pointers/b-pointer-variable/pointer-variable.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+        A variable which stores another variable's memory address is called pointer variable.
+
+        syntax :
+
+        data_type*    variable/pointer_name;
+
+        int *ptr;
+     */
+
+    int x = 10;
+    printf("address of p                          = %p\n", &x);
+
+    int *ptr;
+    ptr = &x;
+    printf("address of p, but now stored in ptr   = %p\n", ptr); // address format specifier is -> %p
+
+    printf("address of ptr itself                 = %p", &ptr); // ptr is variable, it has also its own memory address
+
+    return 0;
+}
