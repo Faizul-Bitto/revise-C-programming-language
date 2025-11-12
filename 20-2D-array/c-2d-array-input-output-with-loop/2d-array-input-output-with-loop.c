44c17eb1e81785e9bf9cf45cee87905debc52c34
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-12 20:49:42 +0600

    feat(2D-array): implement C program for 2D array input and output
    - Add a new C program that allows users to input and output values in a 2D array using loops.
    - Include input and output text files for testing the program's functionality.
    - This addition enhances educational resources related to 2D array manipulation in C programming.

---
+++ 20-2D-array/c-2d-array-input-output-with-loop/2d-array-input-output-with-loop.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int row, column;
+    scanf("%d %d", &row, &column);
+
+    int a[row][column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
